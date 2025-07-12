119615c1a04c70b569e09cef495e784c4505d0b3
tourayalieu54 <tourayalieu54@github.com>
2025-07-12 12:49:47 +0000

    intact again

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-  mongoose.connect(mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-);
+    mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
