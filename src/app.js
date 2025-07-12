8aa93c49cd889de55bad1518bea8087fc3b8c24c
tourayalieu54 <tourayalieu54@github.com>
2025-07-12 16:49:41 +0000

    ***

--- src/app.js
+++ src/app.js
@@ -26,4 +26,6 @@ app.use( authRoutes);
 app.use('/api', employeeRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
-app.listen(8080, () => console.log('Server running on port 8080'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
